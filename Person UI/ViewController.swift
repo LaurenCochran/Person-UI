368a2fc43ff90441ff8153b5c972526ddee0d59f
Lauren <Owner1@MacBooks-MacBook-Pro.local>
2019-02-15 15:46:20 -0600

    First commit

---
+++ Person UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Lauren on 2019-02-15.
+//  Copyright © 2019 Lauren Cochran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
